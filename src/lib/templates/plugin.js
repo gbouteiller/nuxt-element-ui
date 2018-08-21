d5a9aaa30aa6f86ce26f858192f214d018f03027
Gregory Bouteiller <gregory.bouteiller@niama.re>
2018-08-21 10:11:19 +0400

    upgrade

--- src/lib/templates/plugin.js
+++ src/lib/templates/plugin.js
@@ -1,6 +1,23 @@
-<% options.components.forEach((component) => { %>import <%= component.className %> from 'element-ui/lib/<%= component.fileName %>';
-<% }) %>
+<% options.components.forEach((c) => { %>
+import <%= c.className %> from 'element-ui/lib/<%= c.fileName %>';
+<% }); %>
 import Vue from 'vue';
 
-<% options.components.forEach((component) => { %>Vue.use(<%= component.className %>);
-<% }) %>
+<% options.components.forEach((c) => { if (['Loading', 'Message', 'MessageBox', 'Notification'].indexOf(c.className) === -1 ) {%>
+Vue.use(<%= c.className %>);
+<% } else if (c.className === 'Loading' ) { %>
+Vue.use(Loading.directive);
+<% } }); %>
+
+<% options.components.forEach((c) => { if (c.className === 'Loading' ) { %>
+Vue.prototype.$loading = Loading.service;
+<% } else if (c.className === 'MessageBox' ) { %>
+Vue.prototype.$msgbox = MessageBox;
+Vue.prototype.$alert = MessageBox.alert;
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$prompt = MessageBox.prompt;
+<% } else if (c.className === 'Notification' ) { %>
+Vue.prototype.$notify = Notification;
+<% } else if (c.className === 'Message' ) { %>
+Vue.prototype.$message = Message;
+<% } }); %>
